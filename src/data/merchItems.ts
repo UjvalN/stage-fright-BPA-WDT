3095c072a6de73ec266d1b3f0ad026f08aded15b
UjvalN <ujval.nallur@gmail.com>
2024-12-18 16:17:49 -0600

    FINALLYY!!!

--- src/data/merchItems.ts
+++ src/data/merchItems.ts
@@ -1,4 +1,9 @@
 import tee from '../../images/t-shirt_extended.png';
+import hoodie from '../../images/hoodie-extended.png';
+import vinyl from '../../images/midnight_echoes_extended.png';
+import poster from '../../images/tour_ok.png';
+import cap from '../../images/hat_extended.png';
+import picks from '../../images/pick.png';
 
 export const merchItems = [
   {
@@ -13,7 +18,7 @@ export const merchItems = [
     id: 'hoodie-1',
     name: 'Stage Fright Hoodie',
     price: 49.99,
-    image: '/images/hoodie-extended.png',
+    image: hoodie,
     category: 'Apparel',
     sizes: ['S', 'M', 'L', 'XL', 'XXL']
   },
@@ -21,28 +26,28 @@ export const merchItems = [
     id: 'vinyl-1',
     name: 'Limited Edition Vinyl',
     price: 34.99,
-    image: '/images/midnight_echoes_extended.png',
+    image: vinyl,
     category: 'Music'
   },
   {
     id: 'poster-1',
     name: 'Tour Poster',
     price: 19.99,
-    image: '/images/tour_ok.png',
+    image: poster,
     category: 'Accessories'
   },
   {
     id: 'cap-1',
     name: 'Band Logo Cap',
     price: 24.99,
-    image: '/images/hat_extended.png',
+    image: cap,
     category: 'Accessories'
   },
   {
     id: 'picks-1',
     name: 'Guitar Pick Set',
     price: 9.99,
-    image: '/images/pick.png',
+    image: picks,
     category: 'Accessories'
   }
 ];
